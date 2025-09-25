3168efdb5726f653e5e4848ba5c9b9fac0a07126
Danil Lukonin <oldblacktree@gmail.com>
2025-09-25 17:23:40 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Timer from "./components/Timer/Timer";
-import CharacterPicker from "./components/CharPicker/Charpicker";
-
-const characters = [
-  {
-    id: 1,
-    name: "Washerwoman",
-    nameRu: "Прачка",
-    imageSrc: "/assets/Washerwoman.png",
-    role: "citizen",
-  },
-  {
-    id: 2,
-    name: "Monk",
-    nameRu: "Монах",
-    imageSrc: "/assets/Monk.png",
-    role: "citizen",
-  },
-];
+import { roleLayout, demon, minion, outsider, townsfolk } from "./constant";
 
 function App() {
-  const [selectedCharacters, setSelectedCharacters] = useState([]);
-
-  const handleSelectionChange = (selectedIds) => {
-    setSelectedCharacters(selectedIds);
-  };
-
   return (
     <>
       <h1>Blood on the Clocktower</h1>
       <Timer />
-      <div>
-        <h1>Выберите персонажей для игры</h1>
-        <CharacterPicker
-          characters={characters}
-          onSelectionChange={handleSelectionChange}
-        />
-        <div style={{ marginTop: "20px" }}>
-          <h2>Выбранные персонажи ({selectedCharacters.length}):</h2>
-          <ul>
-            {selectedCharacters.map((id) => {
-              const char = characters.find((c) => c.id === id);
-              return <li key={id}>{char.name}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
+      <input type="number" />
     </>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,127 @@
+export const townsfolk = [
+  {
+    name: "Investigator",
+    nameRu: "Сыщик",
+  },
+  {
+    name: "Washerwoman",
+    nameRu: "Прачка",
+  },
+  {
+    name: "Clockmaker",
+    nameRu: "Часовщик",
+  },
+  {
+    name: "Empath",
+    nameRu: "Эмпат",
+  },
+  {
+    name: "Fortuneteller",
+    nameRu: "Гадалка",
+  },
+  {
+    name: "Undertaker",
+    nameRu: "Гробовщик",
+  },
+  {
+    name: "Monk",
+    nameRu: "Монах",
+  },
+  {
+    name: "Gambler",
+    nameRu: "Азартный игрок",
+  },
+  {
+    name: "Preacher",
+    nameRu: "Проповедник",
+  },
+  {
+    name: "Seamstress",
+    nameRu: "Швея",
+  },
+  {
+    name: "Princess",
+    nameRu: "Принцесса",
+  },
+  {
+    name: "Ravenkeeper",
+    nameRu: "Смотритель воронов",
+  },
+  {
+    name: "Tealady",
+    nameRu: "Травница",
+  },
+];
+
+export const outsider = [
+  {
+    name: "Drunk",
+    nameRu: "Пьяница",
+  },
+  {
+    name: "Recluse",
+    nameRu: "Затворник",
+  },
+  {
+    name: "Sweetheart",
+    nameRu: "Душечка",
+  },
+  {
+    name: "Lunatic",
+    nameRu: "Безумец",
+  },
+];
+
+export const minion = [
+  {
+    name: "Godfather",
+    nameRu: "Крестный отец",
+  },
+  {
+    name: "Assassin",
+    nameRu: "Убийца",
+  },
+  {
+    name: "Widow",
+    nameRu: "Вдова",
+  },
+  {
+    name: "Devilsadvocate",
+    nameRu: "Адвокат дьявола",
+    outsiderMod: [-1, 1],
+  },
+];
+
+export const demon = [
+  {
+    name: "Imp",
+    nameRu: "Черт",
+  },
+  {
+    name: "Fanggu",
+    nameRu: "Фан Гу",
+    outsiderMod: 1,
+  },
+  {
+    name: "Pukka",
+    nameRu: "Пукка",
+  },
+  {
+    name: "Nodashii",
+    nameRu: "Но Даши",
+  },
+];
+
+export const roleLayout = {
+  5: [1, 1, 0, 3],
+  6: [1, 1, 1, 3],
+  7: [1, 1, 0, 5],
+  8: [1, 1, 1, 5],
+  9: [1, 1, 2, 5],
+  10: [1, 2, 0, 7],
+  11: [1, 2, 1, 7],
+  12: [1, 2, 2, 7],
+  13: [1, 3, 0, 9],
+  14: [1, 3, 1, 9],
+  15: [1, 3, 2, 9],
+};
